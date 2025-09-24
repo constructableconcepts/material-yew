acba9b6010ffedc207d9a4905ccd68f459dbebaf
constructableconcepts <constructableconcepts@gmail.com>
2025-09-24 08:17:40 +0000

    Fixed props for new components

--- matdemo/src/pages.rs
+++ matdemo/src/pages.rs
@@ -1,3 +1,23 @@
+            <h2>{ "Chips" }</h2>
+            <Chips label={Some("Chips Group".to_string())} selected={false} disabled={false} />
+
+            <h2>{ "Color" }</h2>
+            <Color value={Some("#2196f3".to_string())} disabled={false} />
+
+            <h2>{ "Dialog" }</h2>
+            <Dialog open={false} heading={Some("Dialog Heading".to_string())}>{ "Dialog content here." }</Dialog>
+
+            <h2>{ "Divider" }</h2>
+            <Divider inset={false} vertical={false} />
+
+            <h2>{ "Elevation" }</h2>
+            <Elevation level={Some(3)} />
+
+            <h2>{ "Field" }</h2>
+            <Field label={Some("Field Label".to_string())} value={Some("Field Value".to_string())} disabled={false}>{ "Field content" }</Field>
+
+            <h2>{ "Focus" }</h2>
+            <Focus disabled={false}>{ "Focusable content" }</Focus>
 use yew::prelude::*;
 use material_yew::*;
 
@@ -5,44 +25,98 @@ use material_yew::*;
 pub fn demo_pages() -> Html {
     html! {
         <div>
-            <h2>{ "Button Demo" }</h2>
-            <Button label="Click me!" />
+            <h2>{ "Button" }</h2>
+            <Button variant={ButtonVariants::Filled}>{ "Click me!" }</Button>
 
-            <h2>{ "Checkbox Demo" }</h2>
-            <Checkbox checked={true} />
+            <h2>{ "Checkbox" }</h2>
+            <Checkbox />
 
-            <h2>{ "Chip Demo" }</h2>
-            <Chip label="Demo Chip" />
+            <h2>{ "Chip" }</h2>
+            <Chip variant={ChipVariants::Assist} label={"Demo Chip"} />
 
-            <h2>{ "FAB Demo" }</h2>
-            <Fab icon="add" />
+            <h2>{ "Chips" }</h2>
+            <Chips label={Some("Chips Group".to_string())} />
 
-            <h2>{ "Icon Button Demo" }</h2>
-            <IconButton icon="favorite" />
+            <h2>{ "Circular Progress" }</h2>
+            <CircularProgress value={50} max={100} />
 
-            <h2>{ "Linear Progress Demo" }</h2>
-            <LinearProgress progress={0.5} />
+            <h2>{ "Color" }</h2>
+            <Color value={Some("#2196f3".to_string())} />
 
-            <h2>{ "List Demo" }</h2>
+            <h2>{ "Dialog" }</h2>
+            <Dialog open={false} heading={Some("Dialog Heading".to_string())}>{ "Dialog content here." }</Dialog>
+
+            <h2>{ "Divider" }</h2>
+            <Divider />
+
+            <h2>{ "Elevation" }</h2>
+            <Elevation level={Some(3)} />
+
+            <h2>{ "FAB" }</h2>
+            <Fab variant={FabVariants::Standard} label={"Add"} />
+
+            <h2>{ "Field" }</h2>
+            <Field label={Some("Field Label".to_string())}>{ "Field content" }</Field>
+
+            <h2>{ "Focus" }</h2>
+            <Focus>{ "Focusable content" }</Focus>
+
+            <h2>{ "Icon" }</h2>
+            <Icon icon={Some("star".to_string())} />
+
+            <h2>{ "Icon Button" }</h2>
+            <IconButton variant={IconButtonVariants::Standard}>{ "favorite" }</IconButton>
+
+            <h2>{ "Linear Progress" }</h2>
+            <LinearProgress value={50} max={100} />
+
+            <h2>{ "List" }</h2>
             <List>
                 <ListItem>{ "Item 1" }</ListItem>
                 <ListItem>{ "Item 2" }</ListItem>
             </List>
 
-            <h2>{ "Menu Demo" }</h2>
+            <h2>{ "List Item" }</h2>
+            <ListItem>{ "Standalone List Item" }</ListItem>
+
+            <h2>{ "Menu" }</h2>
             <Menu>
                 <MenuItem>{ "Menu Item 1" }</MenuItem>
                 <MenuItem>{ "Menu Item 2" }</MenuItem>
             </Menu>
 
-            <h2>{ "Radio Demo" }</h2>
-            <Radio checked={true} />
+            <h2>{ "Menu Item" }</h2>
+            <MenuItem>{ "Standalone Menu Item" }</MenuItem>
 
-            <h2>{ "Slider Demo" }</h2>
-            <Slider value={50} min={0} max={100} />
+            <h2>{ "Progress" }</h2>
+            <Progress progress={Some(0.7)} />
 
-            <h2>{ "Switch Demo" }</h2>
-            <Switch checked={true} />
+            <h2>{ "Radio" }</h2>
+            <Radio />
+
+            <h2>{ "Ripple" }</h2>
+            <Ripple>{ "Rippled content" }</Ripple>
+
+            <h2>{ "Select" }</h2>
+            <Select label={Some("Select Label".to_string())}>
+                <MenuItem>{ "Option 1" }</MenuItem>
+                <MenuItem>{ "Option 2" }</MenuItem>
+            </Select>
+
+            <h2>{ "Slider" }</h2>
+            <Slider />
+
+            <h2>{ "Sub Menu" }</h2>
+            <SubMenu>{ "Sub Menu Content" }</SubMenu>
+
+            <h2>{ "Switch" }</h2>
+            <Switch selected={true} />
+
+            <h2>{ "Tabs" }</h2>
+            <Tabs active_index={Some(0)}>{ "Tab content" }</Tabs>
+
+            <h2>{ "TextField" }</h2>
+            <TextField label={Some("Text Field".to_string())} />
         </div>
     }
 }
